0c753e160c50141afe577426954dc20608ce4224
Faiq Azam <bsf1904201@ue.edu.pk>
2025-07-23 01:57:41 +0200

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,25 +1,25 @@
 i=0
-print("Welcome to the Number Guessing Game.
+print("""Welcome to the Number Guessing Game.
 You have 5 attempts to Guess the Number.
-Hint: The number is between 1 and 50.")
-
+Hint: The number is between 1 and 50.""")
+import random
+answer = random.randint(1, 50)
 while i<5:
     i=i+1
     guess=int(input('Give Your Guess:'))
-    if guess==24: 
-        print('You gave the right answer. You Won!')
+    if guess==answer: 
+        print('You Won!')
         break
-    if 14<guess<24:
+    if guess<answer:
         print('Low Value')
         print('try again')
-    if guess<=14:
-        print('Too Low Value')
-        print('try again')
-    if 34>guess>24:
+    
+    if guess>answer:
         print('High Value')
         print('try again')
-    if guess=>34:
-        print('Too High Value')
-        print('try again')
+    
 else:
-    print('Attempts finished!')
+    print('Attempts finished!' \
+    'The number was:', answer)
+    print('Better luck next time!')
+print('Thanks for playing!')
